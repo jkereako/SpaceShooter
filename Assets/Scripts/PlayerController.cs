fdf05c5688d053e34d546112e437802e7e6a1a17
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-12-28 15:06:28 -0500

    Support shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,18 @@ public class PlayerController: MonoBehaviour {
   public float speed;
   public float tilt;
   public Boundry boundry;
+  public GameObject shot;
+  public Transform shotSpawn;
+  public float fireRate;
+
+  float nextFire;
+
+  void Update() {
+    if (Input.GetButton("Fire1") && Time.time > nextFire) {
+      nextFire = Time.time + fireRate;
+      Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+    }
+  }
 
   void FixedUpdate() {
     float horizontalMovement = Input.GetAxis("Horizontal");
