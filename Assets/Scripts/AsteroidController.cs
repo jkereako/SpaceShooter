8666911d79cf79b1d5def05fcc173b100d2bbae4
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-12-28 15:33:19 -0500

    Add asteroid

---
+++ Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AsteroidController: MonoBehaviour {
+  public float tumbleValue;
+
+  void Start() {
+    // Set a random value to the asteroid
+    GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumbleValue;
+  }
+
+  void OnTriggerEnter(Collider other) {
+    if (other.CompareTag("Boundary")) {
+      return;
+    }
+
+    // Destroy the laser shot
+    Destroy(other.gameObject);
+
+    // Destroy the asteroid
+    Destroy(gameObject);
+  }
+}
