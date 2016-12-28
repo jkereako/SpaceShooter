7d452a210c13d7085eeb2d7ec24e9589156a2e18
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-12-28 09:28:28 -0500

    Add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController: MonoBehaviour {
+  public float speed;
+  public Boundry boundry;
+
+  void FixedUpdate() {
+    float horizontalMovement = Input.GetAxis("Horizontal");
+    float verticalMovement = Input.GetAxis("Vertical");
+    Rigidbody aRigidBody = GetComponent<Rigidbody>();
+    Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+    Vector3 position = new Vector3(
+                         Mathf.Clamp(aRigidBody.position.x, boundry.xMin, boundry.xMax), 
+                         0.0f, 
+                         Mathf.Clamp(aRigidBody.position.z, boundry.zMin, boundry.zMax)
+                       );
+
+    aRigidBody.velocity = movement * speed;
+    aRigidBody.position = position;
+  }
+}
+
+[System.Serializable]
+public class Boundry {
+  public float xMin, xMax, zMin, zMax;
+}
